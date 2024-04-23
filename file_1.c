ea327d502e8e84d170cadf1e0dafa82c1c62bae7
Sravani8747 <sravanipasupuleti16@gmail.com>
2024-04-23 16:59:18 +0530

    adding new function

--- file_1.c
+++ file_1.c
@@ -7,13 +7,13 @@ int main() {
     scanf("%d %d", &number1, &number2);
 
     // calculate the sum
-    sum = number1 + number2;      
+    sum = number1 - number2;      
     
-    printf("%d + %d = %d", number1, number2, sum);
+    printf("%d - %d = %d", number1, number2, sum);
     return 0;
 }
 //function added in branch1
 int func_add(int c, int d)
 { 
-	return c+d;
+	return c-d;
 }
